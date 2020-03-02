dc98beb335d3a4dc09b137a3995438474f33a036
Mahesh Guggilla <maheshchari@gmail.com>
2020-03-02 15:12:42 +0530

    change loading url

--- main.ts
+++ main.ts
@@ -27,9 +27,9 @@ function createWindow(): BrowserWindow {
     require('electron-reload')(__dirname, {
       electron: require(`${__dirname}/node_modules/electron`)
     });
-    win.loadURL('https://atlantis-dev4-dashboard.quantela.com/#/login?client=prague-city.com');
+    win.loadURL('https://atlantis-dev4-dashboard.quantela.com/#/login');
   } else {
-    win.loadURL('https://atlantis-dev4-dashboard.quantela.com/#/login?client=prague-city.com');
+    win.loadURL('https://atlantis-dev4-dashboard.quantela.com/#/login');
   }
 
   if (serve) {
